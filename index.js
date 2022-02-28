a7b1284e66ba11431b6f1499d6ce6375b8760ef9
hirotoyoshidome <yoshidome.hiroto@bizpa.co.jp>
2022-02-28 18:43:17 +0900

    add handling cursor

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const imageForm = document.getElementById("image");
 const detailArea = document.getElementById("detail");
 const previewBoard = document.getElementById("preview");
 const previewContext = previewBoard.getContext("2d");
+var offsetX = null;
+var offsetY = null;
+var imageData = null;
 
+// events.
 imageForm.onchange = getDetail;
+previewBoard.onmousemove = getCurrentInfo;
 
 /**
  * on change event.
  */
 function getDetail(e) {
   const file = e.target.files[0];
-  showDetail(file);
   showPreview(file);
+  showDetail(file);
 }
 
 /**
@@ -37,6 +42,15 @@ function showPreview(f) {
       previewBoard.width = img.width;
       previewBoard.height = img.height;
       previewContext.drawImage(img, 0, 0);
+      // image info.
+      createPTag("IMAGE WIDTH", img.width);
+      createPTag("IMAGE HEIGHT", img.height);
+      // get offset.
+      const BB = previewBoard.getBoundingClientRect();
+      offsetX = BB.left;
+      offsetY = BB.top;
+      // get image data.
+      imageData = getImageArray();
     };
     img.src = e.target.result;
   };
@@ -50,4 +64,39 @@ function createPTag(label, text) {
   const pTag = document.createElement("p");
   pTag.textContent = `${label}: ${text}`;
   detailArea.append(pTag);
+}
+
+/**
+ * get image as array.
+ */
+function getImageArray() {
+  const data = previewContext.getImageData(
+    0,
+    0,
+    previewBoard.width,
+    previewBoard.height
+  ).data;
+  const imageArray = [];
+  for (let i = 0; i < data.length; i += 4) {
+    imageArray.push({
+      r: data[i],
+      g: data[i + 1],
+      b: data[i + 2],
+      a: data[i + 3],
+    });
+  }
+  return imageArray;
+}
+
+/**
+ * get current info.
+ */
+function getCurrentInfo(e) {
+  e.preventDefault();
+  if (imageData !== null) {
+    let mouseX = parseInt(e.clientX - offsetX);
+    let mouseY = parseInt(e.clientY - offsetY);
+    console.log(`x : ${mouseX}, y : ${mouseY}`);
+    console.log(imageData);
+  }
 }
